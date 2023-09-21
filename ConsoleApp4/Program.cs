0c2bb42fff8851a0199491d489842505904b35b9
CarlosAngeles19 <carlosangeles.asc@gmail.com>
2023-09-20 21:33:14 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ingrese el numero de notas :");
+        string numerotxt = Console.ReadLine();
+        int numero =int.Parse(numerotxt);
+
+        int sumadenotas = 0;
+        int promedio = 0;
+        
+        for (int i = 1; i <= numero; i++)
+        {
+            Console.WriteLine("ingrese la nota :");
+            int nota= int.Parse(Console.ReadLine());
+            sumadenotas += nota;
+      
+        }
+        promedio=sumadenotas/numero;
+        Console.WriteLine("El promedio es :" + promedio);
+        Console.ReadKey();
+        
+        
+
+        
+
+
+    }
+}
